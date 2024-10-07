29487186cf79362ac4cc77d5757312e670f85eb5
avilkinnick <42978047+avilkinnick@users.noreply.github.com>
2024-10-07 10:39:03 +0300

    Update ObjectsRef.cpp

--- src/ObjectsRef.cpp
+++ src/ObjectsRef.cpp
@@ -13,6 +13,36 @@ ObjectsRef::ObjectsRef(std::string_view full_dmd_route_path)
     {
         throw std::runtime_error { "Failed to open objects.ref" };
     }
+
+    bool mipmap { false };
+    bool smooth { false };
+
+    std::string line {};
+    while (std::getline(file, line))
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+        else if (line.front() == '[')
+        {
+            if (line == "[mipmap]")
+            {
+                mipmap = true;
+            }
+            else if (line == "[not_mipmap]")
+            {
+                mipmap = false;
+            }
+            else if (line == "[smooth]")
+            {
+                smooth = true;
+            }
+            else if (line == "[not_smooth]")
+            {
+            }
+        }
+    }
 }
 
 std::istream& operator>>(std::istream& in, ObjectsRef::Element& element)
